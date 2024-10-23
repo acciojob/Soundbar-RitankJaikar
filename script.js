c5916f000cb6ad2e68cddd30f191914aebfcd4d7
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 06:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-//your JS code here. If required.
+/*
+const sound = button.getAttribute("data-sound");
+// Create a new audio element and append it to the DOM
+currentAudio = document.createElement("audio");
+currentAudio.src = sound;
+document.body.appendChild(currentAudio);  // Append the audio element to the body
+//console.log(currentAudio);
+*/
+
+
 const buttons= document.querySelectorAll(".btn");
 const stop= document.querySelector(".stop");
 
 let currentAudio= null;
 
 buttons.forEach(button => {
-	/*
-	const sound = button.getAttribute("data-sound");
-	// Create a new audio element and append it to the DOM
-	currentAudio = document.createElement("audio");
-	currentAudio.src = sound;
-	document.body.appendChild(currentAudio);  // Append the audio element to the body
-	//console.log(currentAudio);
-	*/
-
 	button.addEventListener("click", () => {
 		const sound = button.getAttribute("data-sound");
 		
@@ -23,6 +23,7 @@ buttons.forEach(button => {
 		}
 		
 		currentAudio = new Audio(sound);
+		document.body.appendChild(currentAudio);  // Append the audio element to the body
 		currentAudio.play();
 	})
 })
