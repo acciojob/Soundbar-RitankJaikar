e21c1960aec511d43bedd6fee4c76251868fdf7c
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 05:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+const buttons= document.querySelectorAll(".btn");
+const stop= document.querySelector(".stop");
+
+let currentAudio= null;
+
+buttons.forEach(button => {
+	button.addEventListener("click", () => {
+		const sound = button.getAttribute("data-sound");
+		if(currentAudio) {
+			currentAudio.pause();
+			currentAudio.currentTime = 0;
+		}
+		currentAudio = new Audio(sound);
+		currentAudio.play();
+	})
+})
+
+stop.addEventListener("click", () => {
+	if(currentAudio) {
+		currentAudio.pause();
+		currentAudio.currentTime = 0;
+	}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
