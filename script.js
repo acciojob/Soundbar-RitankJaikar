66bc268ff42376c7f1cd52296a9dc5d3d6fde06a
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 06:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const stop= document.querySelector(".stop");
 let currentAudio= null;
 
 buttons.forEach(button => {
+	/*
 	const sound = button.getAttribute("data-sound");
 	// Create a new audio element and append it to the DOM
 	currentAudio = document.createElement("audio");
 	currentAudio.src = sound;
+	*/
 	document.body.appendChild(currentAudio);  // Append the audio element to the body
-	console.log(currentAudio);
-	
+	//console.log(currentAudio);
+
 	button.addEventListener("click", () => {
+		const sound = button.getAttribute("data-sound");
+		
 		if(currentAudio) {
 			currentAudio.pause();
 			currentAudio.currentTime = 0;
